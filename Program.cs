d7f8dd48adae51433c9f60958051ed4bd2a33ffb
Shumkov Artem <t_shumkov.2020@mail.ru>
2024-02-17 21:26:03 +0300

    Добавил решение в папку program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Result(string[] onestr, string[] twostr)
+{
+    int count = 0;
+    for (int i = 0; i < onestr.Length; i++)
+    {
+    if(onestr[i].Length <= 3)
+        {
+        twostr[count] = onestr[i];
+        count++;
+        }
+    }
+}
+
+void Printstr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] onestr = new string [6] {"hello", "-2", "world", "-)", "Denmark", "123"};
+string[] twostr = new string[onestr.Length];
+Console.WriteLine( "Первоначальный массив:");
+Printstr(onestr);
+Result(onestr, twostr);
+Console.WriteLine( "Массив из строк меньше, либо равных трем:");
+Printstr(twostr);
